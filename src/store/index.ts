2201106f7a655a05cee60f3003eb25f7aa21f549
dikobrazik <ilnar.khusaenov@gmail.com>
2021-05-15 20:00:45 +0300

    feat(store): base store created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import tasksStore from '../app/modules/tasks/store/index/reducer';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    tasksStore,
+  },
 });
+
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
