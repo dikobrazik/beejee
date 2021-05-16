94d702fa0d782a028e045956a17cbe3935fbc999
dikobrazik <ilnar.khusaenov@gmail.com>
2021-05-16 18:24:44 +0300

    feat(tasks@ui): tasks index page improvements (create button, create form and etc.)

--- src/app/modules/tasks/ui/pages/index/index.tsx
+++ src/app/modules/tasks/ui/pages/index/index.tsx
@@ -1,26 +1,49 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, Col, Modal, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../../../../../../store';
 import CommonList from '../../../../common/ui/components/list';
 import Loader from '../../../../common/ui/components/loader';
-import { Task } from '../../../domain/interfaces/task';
 import { loadTasks } from '../../../store/index/actions';
 import { isLoadingSelector, tasksSelector } from '../../../store/index/selectors';
-
-const TasksListItem = (task: Task) => {
-  return <div>{task.email}</div>;
-};
+import TaskCardComponent from '../../components/task-card';
+import TasksCreateForm from '../../forms/create';
 
 const TasksPagesIndex = () => {
+  const [isCreateModalShown, setIsCreateModalShown] = useState(false);
+  const handleShow = useCallback(() => setIsCreateModalShown(true), []);
+  const handleClose = useCallback(() => setIsCreateModalShown(false), []);
+
   const dispatch = useDispatch<AppDispatch>();
   const tasks = useSelector(tasksSelector);
   const loading = useSelector(isLoadingSelector);
+
   useEffect(() => {
     dispatch(loadTasks());
   }, []);
   return (
     <>
-      <CommonList data={tasks} renderItem={TasksListItem}></CommonList>
+      <Row className="mb-4">
+        <Col></Col>
+        <Col md="auto">
+          <Button variant="primary" onClick={handleShow}>
+            Добавить задачу
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CommonList data={tasks} renderItem={TaskCardComponent}></CommonList>
+        </Col>
+      </Row>
+      <Modal show={isCreateModalShown} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Создание задачи</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TasksCreateForm onDone={handleClose}></TasksCreateForm>
+        </Modal.Body>
+      </Modal>
       <Loader visible={loading}></Loader>
     </>
   );
